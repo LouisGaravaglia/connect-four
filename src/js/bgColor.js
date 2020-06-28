7302eb69945b9a19c01010fa0bd136b1b37aedeb
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-06-28 08:38:14 -0700

    changed top row to be changing colors as well, and added shadows.

--- src/js/bgColor.js
+++ src/js/bgColor.js
@@ -1,44 +1,45 @@
 var colors = [
-  "#f3d58b",
-  "#f3d59b",
-  "#f3d5ab",
-  "#f3d5bb",
-  "#f3d5cb",
-  "#e3d5cb",
-  "#d3d5cb",
-  "#c3d5cb",
-  "#b3d5cb",
-  "#a3d5cb",
-  "#93d5cb",
-  "#83d5cb",
-  "#73d5cb",
-  "#83d5cb",
-  "#93d5cb",
-  "#a3d5cb",
-  "#b3d5cb",
-  "#c3d5cb",
-  "#d3d5cb",
-  "#e3d5cb",
-  "#f3d5cb",
-  "#f3d5bb",
-  "#f3d5ab",
-  "#f3d59b",
-  "#f3d58b",
+  "#21537f",
+  "#31537f",
+  "#41537f",
+  "#51537f",
+  "#61537f",
+  "#71537f",
+  "#61537f",
+  "#51537f",
+  "#41537f",
+  "#31537f",
+  "#21537f",
+  "#11537f",
 ];
 
-let galleryBG = document.querySelector(".background");
+var colors2 = [
+  "#214185",
+  "#314185",
+  "#414185",
+  "#514185",
+  "#614185",
+  "#714185",
+  "#614185",
+  "#514185",
+  "#414185",
+  "#314185",
+  "#214185",
+  "#114185",
+];
 
 var currentColor = 0;
+
 const switchColor = () => {
   let homeBG = document.querySelector(".background");
+  let topColumn = document.getElementById("column-top");
 
   if (currentColor >= colors.length) currentColor = 0;
-  console.log(currentColor);
 
-  // $(".background").css("background-color", colors[currentColor++]);
   homeBG.style.background = colors[currentColor++];
-  console.log(homeBG);
-  setTimeout(switchColor, 2000);
+  topColumn.style.background = colors2[currentColor++];
+
+  setTimeout(switchColor, 4000);
 };
 
 switchColor();
